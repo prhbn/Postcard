1a0127e7638697db309296369661b27153c7a849
prhbn <parthiban.m@me.com>
2015-05-26 22:38:25 -0700

    Second rev of project

---
+++ LearningSwift/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  LearningSwift
+//
+//  Created by Parthiban Mohanraj on 5/25/15.
+//  Copyright (c) 2015 Parthiban Mohanraj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var mailButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        
+        messageLabel.hidden = false
+        messageLabel.textColor = UIColor.redColor()
+        
+        //Combine both the text fields and print the output in the UILabel
+        var statement: String = "\(enterNameTextField.text) says \(enterMessageTextField.text)"
+        
+        messageLabel.text = statement
+        enterNameTextField.text = ""
+        enterMessageTextField.text = ""
+    
+        enterMessageTextField.resignFirstResponder()
+        
+        mailButton.setTitle("Sent Message", forState: UIControlState.Normal)
+        
+    }
+    
+    
+
+}
+
